57b08c155d06cc2a3e560c8d8addb318b34a0b84
dagoret <dagoret@lal.in2p3.fr>
2022-01-11 00:59:37 +0100

    correct flake8 a 2nd time

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ import os
 import math
 
 
-def func1(a , b) -> str:
+def func1(a, b) -> str:
     return math.floor(a + b)
 
 
-def func2(a , b, c) -> int:
+def func2(a, b, c) -> int:
     return os.getcwd()
